91204a829e8adbba67d97af26478930d9d7c4ffe
faisal-kursheedali <85350108+faisal-kursheedali@users.noreply.github.com>
2022-09-25 06:34:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch} from "react-redux";
 import "./App.css";
 import { loadAuth } from "./app/feature/auth";
 import { Footer, Nav } from "./component";
